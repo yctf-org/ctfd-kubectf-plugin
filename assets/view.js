0950b6734e7682e82319b0a6ebea34c7f038b708
scrash <admin@xdb.be>
2023-02-26 21:43:28 +0100

    update view with alpinejs for compat with newer CTFd

--- assets/view.js
+++ assets/view.js
@@ -1,187 +1,191 @@
 CTFd._internal.challenge.data = undefined
 
-CTFd._internal.challenge.renderer = CTFd.lib.markdown();
+CTFd._internal.challenge.renderer = null;
 
 
 CTFd._internal.challenge.preRender = function () { }
 
-CTFd._internal.challenge.render = function (markdown) {
-    return CTFd._internal.challenge.renderer.render(markdown)
-}
+CTFd._internal.challenge.render = null;
 
 
 CTFd._internal.challenge.postRender = function () { }
 
 
-CTFd._internal.challenge.submit = function (preview) {
-    var challenge_id = parseInt(CTFd.lib.$('#challenge-id').val())
-    var submission = CTFd.lib.$('#challenge-input').val()
-
+CTFd._internal.challenge.submit = function(preview) {
+    var challenge_id = parseInt(CTFd.lib.$("#challenge-id").val());
+    var submission = CTFd.lib.$("#challenge-input").val();
+  
     var body = {
-        'challenge_id': challenge_id,
-        'submission': submission,
-    }
-    var params = {}
+      challenge_id: challenge_id,
+      submission: submission
+    };
+    var params = {};
     if (preview) {
-        params['preview'] = true
+      params["preview"] = true;
     }
+  
+    return CTFd.api.post_challenge_attempt(params, body).then(function(response) {
+      if (response.status === 429) {
+        // User was ratelimited but process response
+        return response;
+      }
+      if (response.status === 403) {
+        // User is not logged in or CTF is paused.
+        return response;
+      }
+      return response;
+    });
+  };
+  
 
-    return CTFd.api.post_challenge_attempt(params, body).then(function (response) {
-        if (response.status === 429) {
-            // User was ratelimited but process response
-            return response
+function deployment() {
+    return {
+        deploymentName: null,
+        danger: false,
+        alert: null,
+        challengeCreateVisible: false,
+        challengeUpdateVisible: false,
+        createLoading: false,
+        extendLoading: false,
+        terminateLoading: false,
+        expires: null,
+        host: null,
+        setDanger() { this.danger = true },
+        unsetDanger() { this.danger = false },
+        isDanger() { return this.danger === true },
+        setAlertText(text) { this.alert = text},
+        resetAlert() {
+            this.setAlertText(null);
+            this.unsetDanger();
+        },
+        toggleChallengeCreate() {
+            this.challengeCreateVisible = !this.challengeCreateVisible;
+        },
+        toggleChallengeUpdate() {
+            this.challengeUpdateVisible = !this.challengeUpdateVisible;
+        },
+        toggleCreateLoading() {
+            this.createLoading = !this.createLoading;
+        },
+        toggleExtendLoading() {
+            this.extendLoading = !this.extendLoading;
+        },
+        toggleTerminateLoading() {
+            this.terminateLoading = !this.terminateLoading;
+        },
+        getDeployment(deploymentName) {
+            this.deploymentName = deploymentName;
+            this.resetAlert();
+            fetch("api/kube_ctf/" + deploymentName).then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
+                        this.createChallengeLinkElement(data);
+                        this.toggleChallengeUpdate();
+                    });
+                } else {
+                    this.setAlertText("Challenge not started");
+                    this.toggleChallengeCreate();
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        createDeployment() {
+            this.toggleCreateLoading();
+            this.resetAlert();
+            fetch("api/kube_ctf/" + this.deploymentName, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({action: "create"})
+            }).then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
+                        this.createChallengeLinkElement(data);
+                        this.toggleChallengeUpdate();
+                        this.toggleChallengeCreate();
+                        this.toggleCreateLoading();
+                    });
+                } else {
+                    response.json().then(error => {
+                        this.setAlertText(error.error || error.message);
+                        this.setDanger();
+                        this.toggleCreateLoading();
+                    });
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        createChallengeLinkElement(data) {
+            this.expires = new Date(data.deployment.expires);
+            expiry = calculateExpiry(new Date(data.deployment.expires));
+            if (expiry > 0) {
+                this.host = data.deployment.host;
+            } 
+        },
+        extendDeployment() {
+            this.toggleExtendLoading();
+            this.resetAlert();
+            fetch("api/kube_ctf/" + this.deploymentName, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({action: "extend"})
+            }).then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
+                        this.createChallengeLinkElement(data);
+                        this.toggleExtendLoading();
+                    });
+                } else {
+                    response.json().then(error => {
+                        this.setAlertText(error.error || error.message);
+                        this.setDanger();
+                        this.toggleExtendLoading();
+                    });
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        terminateDeployment() {
+            this.toggleTerminateLoading();
+            this.resetAlert();
+            fetch("api/kube_ctf/" + this.deploymentName, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({action: "terminate"})
+            }).then(response => {
+                if (response.ok) {
+                    this.toggleChallengeUpdate();
+                    this.toggleTerminateLoading();
+                    this.setAlertText("Challenge Terminated.");
+                    this.expires = null;
+                    this.host = null;
+                    this.toggleChallengeCreate();
+                } else {
+                    response.json().then(error => {
+                        this.setAlertText(error.error || error.message);
+                        this.setDanger();
+                        this.toggleTerminateLoading();
+                    });
+                }
+            }).catch(error => {
+                console.log(error);
+            });
         }
-        if (response.status === 403) {
-            // User is not logged in or CTF is paused.
-            return response
-        }
-        return response
-    })
-};
 
-function toggleLoading(btn) {
-    var icon = btn.querySelector('i');
-    btn.disabled = !btn.disabled;
-    icon.classList.toggle('fa-spin');
-    icon.classList.toggle('fa-spinner');
+    }
 }
 
-function resetAlert() {
-    let alert = $(".deployment-actions > .alert").first();
-    alert.empty();
-    alert.removeClass("alert-danger");
-    return alert;
-}
-
-function toggleChallengeCreate() {
-    let btn = $(".create-chal").first();
-    btn.toggleClass('d-none');
-}
-
-function toggleChallengeUpdate() {
-    let btn = $(".extend-chal").first();
-    btn.toggleClass('d-none');
-
-    btn = $(".terminate-chal").first();
-    btn.toggleClass('d-none');
-}
 
 function calculateExpiry(date) {
     // Get the difference in minutes
     let difference = Math.floor((date - Date.now()) / (1000 * 60));
     return difference;
-}
-
-function createChallengeLinkElement(data, parent) {
-    let expiry = calculateExpiry(new Date(data.expires));
-
-    if (expiry > 0) {
-        var expires = document.createElement('span');
-        expires.textContent = "Expires in " + calculateExpiry(new Date(data.deployment.expires)) + " minutes.";
-        var link = document.createElement('a');
-        link.href = 'https://' + data.deployment.host;
-        link.textContent = data.deployment.host;
-        parent.append(expires);
-        parent.append(document.createElement('br'));
-        parent.append(link);
-    } 
-}
-
-function awaitChallengeReady(data) {
-    
-}
-
-function getDeployment(deployment) {
-    let alert = resetAlert();
-
-    $.ajax({
-        type: "GET",
-        url: "api/kube_ctf/" + deployment,
-        success: function(data) {
-            createChallengeLinkElement(data, alert);
-            toggleChallengeUpdate();
-        },
-        error: function(error) {
-            alert.append("Challenge not started")
-            toggleChallengeCreate();
-        }
-    }) 
-}
-
-function createDeployment(btn) {
-    let deployment = btn.dataset.deployment;
-    toggleLoading(btn);
-    let alert = resetAlert();
-
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "create"}),
-        contentType: "application/json",
-        success: function(data) {
-	    createChallengeLinkElement(data, alert);
-	    toggleChallengeUpdate();
-	    toggleChallengeCreate();
-	    toggleLoading(btn);
-        },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
-            toggleLoading(btn);
-        }
-    }) 
-}
-
-function extendDeployment(btn) {
-    let deployment = btn.dataset.deployment;
-    toggleLoading(btn);
-    let alert = resetAlert();
-
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "extend"}),
-        contentType: "application/json",
-        success: function(data) {
-            createChallengeLinkElement(data, alert)
-            toggleLoading(btn);
-        },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
-            toggleLoading(btn);
-        }
-    })    
-
-}
-
-// function resetDeployment() {
-
-// }
-
-function terminateDeployment(btn) {
-    let deployment = btn.dataset.deployment;
-    toggleLoading(btn);
-    let alert = resetAlert();
-
-    // Can't use the nice format cause need to put content-type header in
-    $.ajax({
-        type: "POST",
-        url: "api/kube_ctf/" + deployment,
-        data: JSON.stringify({action: "terminate"}),
-        contentType: "application/json",
-        success: function(data) {
-            alert.append("Challenge Terminated.")
-            toggleChallengeCreate();
-            toggleChallengeUpdate();
-            toggleLoading(btn);
-        },
-        error: function(error) {
-            alert.append(error.responseJSON.error || error.responseJSON.message)
-            alert.addClass("alert-danger")
-            toggleLoading(btn);
-        }
-    })
 }
